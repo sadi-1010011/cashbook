26ab5053c315ec8d86872bc7db3affbe4c33b0dd
sadiq <sadi1010011@gmail.com>
2024-05-11 04:10:59 +0530

    created event handler functions to new transation input page

--- app/newtransaction/page.tsx
+++ app/newtransaction/page.tsx
@@ -7,61 +7,117 @@ import { useState } from "react";
 export default function NewTransaction() {
 
   const [newtransaction, setNewtransaction] = useState({
-    amount: 0,
-    catogory: "",
-    description: "",
-    transactiontype: ""
+    amount: 0, // got
+    catogory: "", // got 
+    description: "", // got
+    transactiontype: "expense" // trying.., by default new transactoin is expense
   });
 
   const [activetab, setActivetab] = useState(true);
+
+  const transaction_catogories = ["food", "travel", "Entertains", "haircut", "medicine", "others"];
+  const transaction_types = ["income", "expense"]; // debt type coming soon in version 2
 
   return (
     <div className={styles.newtrans_container}>
       <Header />
 
+
       <div className={styles.newamount_card}>
+
+        {/* DATA- TRANSACTION TYPE */}
+
         <div className="w-full flex">
           <div className={styles.currenttab_viewerwrapper}>
-            <div className={styles.active_tab}>Expense</div>
-            <div>Income</div>
+            {
+              transaction_types.map((item, index) => 
+                <div key={index} className={ newtransaction.transactiontype === item ? styles.active_tab : styles.normal_tab} onClick={
+                  e => {
+                    const value = e.currentTarget.textContent;
+                    setNewtransaction(previousdata => {
+                      return {
+                        ...previousdata,
+                        transactiontype: value
+                      }
+                    })
+                  }
+                }>{ item }</div>
+              )
+            }
           </div>
         </div>
+
+        {/* DATA- AMOUNT */}
 
         <input
           className={styles.newinputamt}
           type="number"
           name="amount"
           placeholder="$ Amount.."
-          onChange={ (e) => {
-            // const { value } = e.target;
-            // console.log(value);
+          onChange={ e => {
+            const { value } = e.target;
+            setNewtransaction(previousdata => {
+              return {
+                ...previousdata,
+                amount: value
+              }
+            })
           }}
         />
+
+        {/* DATA- CATOGORY */}
 
         <div className={styles.trans_catogorywrapper}>
           <h3 className="capitalize text-lg font-bold">expenses made for</h3>
           <div className={styles.selectcatogory}>
-            <div>Food</div>
-            <div>Travel</div>
-            <div>Entertains</div>
-            <div>Haircut</div>
-            <div>Medicine</div>
-            <div className="font-bold">+</div>
+            {
+              transaction_catogories.map((item, index) => 
+                <div key={index}
+                    onClick={ (e) => {
+                      let value = e.currentTarget.textContent;
+                      setNewtransaction(previousdata => {
+                        return {
+                          ...previousdata,
+                          catogory: value
+                        }
+                      })  
+                    } }>
+                  {item}
+                </div>
+              )
+            }
           </div>
         </div>
+
+        {/* DATA- DESCRIPTION */}
 
         <div>
           <h3 className="capitalize text-lg font-bold px-4">Description</h3>
           <textarea
             className={styles.newdescription}
             placeholder="descripion.."
+            onChange={ (e) => {
+              const { value } = e.target;
+              setNewtransaction(previousdata => {
+                return {
+                  ...previousdata,
+                  description: value
+                }
+              })
+            }}
           ></textarea>
         </div>
       </div>
 
       <Link href="/dashboard">
-        <button className="text-green-600 py-5 font-bold">Save</button>
+        <button onClick={(newtransaction) => savenewtransaction(newtransaction) } className="text-green-600 py-5 font-bold">Save</button>
       </Link>
     </div>
   );
+}
+
+
+function savenewtransaction(transaction) {
+  // needs to declare type , so learn Typescript well
+  console.log(transaction);
 }
